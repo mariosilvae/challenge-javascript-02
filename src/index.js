04563332106afc4c5acaf0740e7d6c7afef2cbe2
Mario Silva <mario.silvae@outlook.com>
2020-08-17 13:16:39 -0500

    Solved challenge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
+  let arrayFib = [];
 
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || i === 1) {
+      arrayFib[i] = 1;
+    } else if (i > 1) {
+      arrayFib[i] = arrayFib[i - 2] + arrayFib[i - 1];
+    }
   }
+  return arrayFib;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
